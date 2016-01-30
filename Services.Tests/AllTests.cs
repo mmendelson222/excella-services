b518c18582448eeb346b9e12839dc1bdb4b46194
Michael Mendelson <mmendelson@gmail.com>
2016-01-30 09:25:43 -0500

    Hello World

---
+++ Services.Tests/AllTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Services.Rules;
+using System.Collections.Generic;
+using System.Diagnostics;
+using Services.Controllers;
+
+namespace Services.Tests
+{
+    [TestClass]
+    public class AllTests
+    {
+        [TestMethod]
+        public void TestHello()
+        {
+            TestHello(2, new string[] { "Hello World", "Hello World" });
+            TestHello(4, new string[] { "Hello World", "Hello World", "Hello World", "Hello World" });
+        }
+
+        public void TestHello(int input, string[] output)
+        {
+            HelloController gc = new HelloController();
+            CollectionAssert.AreEqual(gc.Post(new int[]{input}), output);
+        }
+    }
+}
---
+++ Services/Controllers/HelloController.cs
@@ -0,0 +1,46 @@
+﻿using Services.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Services.Rules;
+
+namespace Services.Controllers
+{
+    public class HelloController : ApiController
+    {
+        public string[] Post([FromBody] int[] value)
+        {
+            List<string> hellos = new List<string>();
+            for (int i = 0; i < value[0]; i++)
+                hellos.Add("Hello World");
+            return hellos.ToArray();
+        }
+
+
+        // GET: api/Test
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/Demo/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        
+        // PUT: api/Demo/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/Test/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
