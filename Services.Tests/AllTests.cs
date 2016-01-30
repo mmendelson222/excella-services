5750007d099d56d3fffae891450c48b22ef9bd5c
Michael Mendelson <mmendelson@gmail.com>
2016-01-30 09:59:59 -0500

    Fibonacci

--- Services.Tests/AllTests.cs
+++ Services.Tests/AllTests.cs
@@ -11,16 +11,36 @@ namespace Services.Tests
     public class AllTests
     {
         [TestMethod]
-        public void TestHello()
+        public void TestFib()
         {
-            TestHello(2, new string[] { "Hello World", "Hello World" });
-            TestHello(4, new string[] { "Hello World", "Hello World", "Hello World", "Hello World" });
+            //TestHello(2, new int[] { 1 });
+            //TestHello(3, new int[] { 2 });
+            TestHello(4, new int[] { 3 });
+            TestHello(new int[] { 4, 3 },  new int[] { 3, 2 });
         }
 
-        public void TestHello(int input, string[] output)
+        public void TestHello(int input, int[] output)
         {
-            HelloController gc = new HelloController();
-            CollectionAssert.AreEqual(gc.Post(new int[]{input}), output);
+            FibController gc = new FibController();
+            CollectionAssert.AreEqual(gc.Post(new int[] { input }), output);
         }
+
+        public void TestHello(int [] input, int[] output)
+        {
+            FibController gc = new FibController();
+            CollectionAssert.AreEqual(gc.Post(input) , output);
+        }
+        //[TestMethod]
+        //public void TestHello()
+        //{
+        //    TestHello(2, new string[] { "Hello World", "Hello World" });
+        //    TestHello(4, new string[] { "Hello World", "Hello World", "Hello World", "Hello World" });
+        //}
+
+        //public void TestHello(int input, string[] output)
+        //{
+        //    HelloController gc = new HelloController();
+        //    CollectionAssert.AreEqual(gc.Post(new int[]{input}), output);
+        //}
     }
 }
---
+++ Services/Controllers/Fib.cs
@@ -0,0 +1,68 @@
+﻿using Services.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Services.Rules;
+
+namespace Services.Controllers
+{
+    public class FibController : ApiController
+    {
+        public int[] Post([FromBody] int[] value)
+        {
+            List<int> fibs = new List<int>();
+            for (int i = 0; i < value.Length; i++)
+                fibs.Add(Generate(value[i]));
+            return fibs.ToArray();
+        }
+
+        List<int> sequence = new List<int>();
+        private int Generate(int p)
+        {
+            if (sequence.Count < p)
+            {
+                CreateFibSequence(p);
+            }
+
+            return sequence[p - 1];
+        }
+
+        /// <summary>
+        /// create sequence of at least the given length
+        /// </summary>
+        private void CreateFibSequence(int p)
+        {
+            sequence = new List<int>() {1, 1};
+
+            for (int i=2; i<p; i++)
+                sequence.Add(sequence[i-1] + sequence[i-2]);
+        }
+
+
+        // GET: api/Test
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/Demo/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+
+        // PUT: api/Demo/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/Test/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
